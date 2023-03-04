bafe44d4cf7c886cdc5f5bf3c798e190c62943cb
changcity22 <chancity22@gmail.com>
2023-03-03 23:47:28 -0500

    Add sum-of-odd-numbers.js

---
+++ 7-kyu/sum-of-odd-numbers.js
@@ -0,0 +1,38 @@
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+
+function rowSumOddNumbers(n) {
+	
+  let sum = 0;
+  
+  for (let i = lastOdd(n); i >= lastOdd(n) - (n-1) * 2; i -= 2) {
+    sum += i;
+  }
+  
+  return sum;
+  
+}
+
+
+function lastOdd(n) {
+  
+  let num = 0;
+  
+  for( let i = n; i > 1; i--) {
+    num += i;
+  }
+  
+  return 1 + num*2;
+}
